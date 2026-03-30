ee344025b8e605ab63e04639dc07de2ebda950c0
ajah <ajahajang00@gmail.com>
2026-03-30 23:06:09 +0200

    Create a todoSlice

---
+++ reduxTodoapp/src/features.ts/todoSlice.ts
@@ -0,0 +1,17 @@
+import {createSlice} from '@reduxjs/toolkit'
+type Item={id:number,text:string,complete:boolean}
+type initialVal={items:Item[]}
+
+const initialState:initialVal={items:[]}
+
+const todoReducer=createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        addtodo(state,action){
+            state.items.push(action.payload)
+        }
+
+    }
+
+})
