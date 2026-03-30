fe1802d7fd50abd6e1c3a314a8ab6acba14e8d74
ajah <ajahajang00@gmail.com>
2026-03-30 23:22:02 +0200

    Attach AddTodo component to ui

--- reduxTodoapp/src/App.tsx
+++ reduxTodoapp/src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './output.css'
+import AddTodo from './components/AddTodo'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
     <div className='bg-green-100'> hello</div>
+    <AddTodo/>
     </>
   )
 }
