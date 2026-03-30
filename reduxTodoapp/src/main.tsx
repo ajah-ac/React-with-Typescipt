9ec83009694a573d66d056682ac7f4400fa743f9
ajah <ajahajang00@gmail.com>
2026-03-30 22:49:40 +0200

    Configure Store

--- reduxTodoapp/src/main.tsx
+++ reduxTodoapp/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './output.css'
 import App from './App.tsx'
+import {configureStore,} from '@reduxjs/toolkit'
+import {Provider} from 'react-redux'
+const store=configureStore({
+  reducer:{}
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+<Provider store={store}>
+      <App />
+
+</Provider>
   </StrictMode>,
 )
