45c9f05af56d1ec6b8f00b3e619a77bc00d405ad
ajah <ajahajang00@gmail.com>
2026-03-30 22:44:02 +0200

    Setup Tailwind css

--- reduxTodoapp/src/App.tsx
+++ reduxTodoapp/src/App.tsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
-
+import './output.css'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className='bg-green-100'> hello</div>
     </>
   )
 }
