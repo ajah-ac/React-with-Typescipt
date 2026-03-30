8198226048e0b2399e8b912e1b36c28d4fa20e75
ajah <ajahajang00@gmail.com>
2026-03-30 23:21:03 +0200

    create AddTodo Component

---
+++ reduxTodoapp/src/components/AddTodo.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addtodo } from '../features/todoSlice'
+import { Plus } from 'lucide-react'
+import { nanoid } from 'nanoid'
+const AddTodo = () => {
+    const [input, setInput] = useState('')
+    const dispatch = useDispatch()
+    return (
+        <>
+            <div>
+                <input type="text"
+                    placeholder='Add a tasK'
+                    onChange={(e) => setInput(e.target.value)} />
+                <button onClick={() => dispatch(addtodo({ id: nanoid(), text: input, complete: false }))}><Plus />
+                </button>
+            </div>
+
+        </>
+    )
+}
+
+export default AddTodo
