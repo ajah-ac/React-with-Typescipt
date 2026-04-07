fa4e9db825e4ee21420d22e714c10fe59281ef65
ajah <ajahajang00@gmail.com>
2026-04-07 10:25:54 +0200

    Show/Hide password

--- Login/src/App.tsx
+++ Login/src/App.tsx
@@ -2,10 +2,32 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState('')
+   const [pass, setPassWord] = useState('')
+   const [showPass,setShowPass]=useState(false)
+
+   function handleSubmit(e:React.SubmitEvent<HTMLFormElement>){
+    e.preventDefault()
+    if(pass.length<6){
+      alert('Password must be more than 6 characters')
+    }
+
+    
+   }
 
   return (
     <>
+    <form action="" onSubmit={handleSubmit}>
+      <input type="text"
+      onChange={(e)=>setEmail(e.target.value)}
+       name="" id="" 
+       placeholder='Enter Email' />
+    <input type= {showPass?"text":'password'}
+     name="" id="" 
+           onChange={(e)=>setPassWord(e.target.value)}
+      placeholder='Enter Passaword'/>
+<button onClick={()=>{setShowPass(prev=>!prev)}}> {showPass?'Hide':'Show'}
+  </button>    </form>
       
     </>
   )
