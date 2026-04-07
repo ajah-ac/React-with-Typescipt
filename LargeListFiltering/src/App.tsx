036e24b05c52ed77ed27bcfa96cc3c22b0e9c4b8
ajah <ajahajang00@gmail.com>
2026-04-07 10:55:50 +0200

    Filter logic

--- LargeListFiltering/src/App.tsx
+++ LargeListFiltering/src/App.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState,useMemo,useCallback } from 'react'
+
 import './App.css'
-
+import ItemsList  from './Items'
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('')
+const Change=useCallback((e:React.ChangeEvent<HTMLInputElement>)=>{setInput(e.target.value)},[])
 
-  return (
+const filter=useMemo(()=>ItemsList.filter(item=>item.name===input),[input])
+ if(filter.length<0 && input!==''){
+  alert('Item not found')
+ }
+return (
     <>
-      
+      <input type="text"
+       onChange={Change}
+      placeholder='Search for an item...' />
+      <button >Search</button>
+      {<ul>{filter.map((item)=><li key={item.id}>
+        <h1> Item:{item.name}</h1>
+      </li>)}
+        </ul>}
     </>
   )
 }
