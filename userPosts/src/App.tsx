f194d1ece5f0f8125cace3373d3926dcb6dbe4bb
ajah <ajahajang00@gmail.com>
2026-04-07 11:40:25 +0200

    Display user Information

--- userPosts/src/App.tsx
+++ userPosts/src/App.tsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import useUsers from './useUsers'
+import type {Users} from './useUsers'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {status,data,error} = useUsers()
 
   return (
     <>
-      
+           {status === 'loading' && <p> Loading</p>}
+            {status === 'error' && <p> {error}</p>}
+            {status==='success' && <ul>
+{ data.map((user:Users)=><li key={user.id}><h3>{user.name}</h3>
+<p>{user.email}</p>
+</li>)}
+            </ul>}
     </>
   )
 }
--- userPosts/src/useUsers.tsx
+++ userPosts/src/useUsers.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 type fetchStatus = 'loading' | 'success' | 'error'
  export interface Users {
     id: number,
