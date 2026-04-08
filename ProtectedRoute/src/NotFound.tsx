a1e2768e3b535c06aac00ae8a38fbe1caa85d08b
ajah <ajahajang00@gmail.com>
2026-04-08 15:23:42 +0200

    Chore:Create a 404 page

---
+++ ProtectedRoute/src/NotFound.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <><h1>404 Not Found</h1>
+    <p>This is the page that handles all pages not found. </p></>
+  )
+}
+
+export default NotFound
