a3ad76893711ce3abd9bfc1f04d9f94a89cc55c1
ajah <ajahajang00@gmail.com>
2026-04-08 16:23:01 +0200

    Chore:Display user in Dropdown

---
+++ userPostsLoader/src/users.tsx
@@ -0,0 +1,21 @@
+import { useLoaderData } from 'react-router-dom'
+interface User{
+    id:number,
+    name:string
+}
+const Users = () => {
+    const users:User[]=useLoaderData()
+    if(!users){
+        return 'No users found'
+    }
+  return (
+    <select name='users'>
+        <option value=''  disabled >Choose a user </option>
+{users.map(user=><option key={user.id}
+value={user.id}
+> {user.name}</option>)}
+    </select>
+  )
+}
+
+export default Users
