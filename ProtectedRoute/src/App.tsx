c64b3da75fc922fec1814fce4712d03bf89d6333
ajah <ajahajang00@gmail.com>
2026-04-08 15:24:30 +0200

    Chore:Protected Route logic

--- ProtectedRoute/src/App.tsx
+++ ProtectedRoute/src/App.tsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
 import './App.css'
+import {Route} from 'react-router-dom'
+import  AuthRequired  from './AuthRequired';
+import {
+    RouterProvider, 
+    createBrowserRouter, 
+    createRoutesFromElements
+} from 'react-router-dom'
+import Layout
+ from "./Layout";
+import NotFound from './NotFound';
+ const router =createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+<Route index element={<h1>Home Page</h1>}/>
+<Route element={<AuthRequired/>}>
+  <Route path='protected' element={<h1>Super secret info here</h1>}/>
+</Route>
+    <Route path='*' element={<NotFound/>}/>
 
+    </Route>
+ ))
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <RouterProvider router={router}/>
     </>
   )
 }
