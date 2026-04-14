d1ee2a29c58fbd8fdb95376e53b19b737d2e823c
ajah <ajahajang00@gmail.com>
2026-04-14 19:22:57 +0200

    Add Routes

--- LandingTemplate/src/App.tsx
+++ LandingTemplate/src/App.tsx
@@ -1,14 +1,24 @@
-
 import './App.css'
-import {createBrowserRouter,RouterProvider,createRoutesFromElements,Route,useLoaderData} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route, useLoaderData } from 'react-router-dom'
+import Home from './pages/Home.tsx'
+import { Features } from './pages/Features.tsx'
+import Teams from './pages/teams.tsx'
+import Layout from './components/Layout.tsx'
 function App() {
-const router=createBrowserRouter(createRoutesFromElements(<Route>
+  const router = createBrowserRouter(createRoutesFromElements(<Route>
+   
+   <Route element={<Layout/>}>
+    <Route path='/' element={<Home />} />
 
+    <Route path='/home' element={<Home />} />
+    <Route path='/features' element={<Features />} />
+    <Route path='/teams' element={<Teams/>} />
 
+   </Route>
 
-</Route>))
+  </Route>))
   return (
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   )
 }
 
